4d1bbba96b06f5e8c265d2f5a6ec32c3ef3f1611
roscioli <octavio.d.roscioli@vanderbilt.edu>
2015-05-19 04:34:08 -0500

    changed env in indexjs

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var http = require("http");
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -7,6 +6,4 @@ app.get('/', function(request, response){
     response.sendFile('index.html', { root: __dirname });
 });
 
-http.listen(process.env.PORT || 8080, function(){
-  console.log('listening on ', http.address().port);
-});
+app.listen(process.env.PORT || 8080);
